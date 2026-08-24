574fb6306847b798062bba9f3b45f53c25c439b7
SlickBunny <163926968+SlickBunny@users.noreply.github.com>
2026-08-24 10:32:02 -0300

    Update Exercicio2.java

--- Aula_01/Exercicio2.java
+++ Aula_01/Exercicio2.java
@@ -1,1 +1,20 @@
 
+// Impressão utilizando 4 linhas
+public class Ficha {
+    public static void main(String[] args) {
+      
+        System.out.println("Nome: Vinicius");
+        System.out.println("Cidade: Campinas");
+        System.out.println("Curso: Ciência da Computação");
+        System.out.println("Objetivo: Entender a fundo a alocação de memória no Java");
+      
+    }
+}
+
+// Impressão utilizando o \n
+public class Ficha {
+    public static void main(String[] args) {
+
+        System.out.println("Nome: Vinicius\nCidade: Campinas\nCurso: Ciência da Computação\nObjetivo: Entender a fundo a alocação de memória no Java");
+    }
+}
