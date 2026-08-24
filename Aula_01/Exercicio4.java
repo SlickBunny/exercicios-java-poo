d73b517a6e74789274a6ae6cec149b6341470645
SlickBunny <163926968+SlickBunny@users.noreply.github.com>
2026-08-24 10:51:00 -0300

    Create Exercicio4.java

---
+++ Aula_01/Exercicio4.java
@@ -0,0 +1,14 @@
+
+public class Corrigir {
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo!");
+    }
+}
+
+/*
+error: class Consertar is public, should be declared in a file named Consertar.java
+
+
+Regra: Em Java, toda classe pública (public class) deve obrigatoriamente 
+ter o mesmo nome do arquivo .java onde ela está salva (respeitando maiúsculas e minúsculas).
+ */
