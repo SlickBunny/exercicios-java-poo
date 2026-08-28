479f75059c9b289f37265ed2efe01abd4c906738
SlickBunny <163926968+SlickBunny@users.noreply.github.com>
2026-08-28 11:19:05 -0300

    Update Exercicio1.java

--- Aula_02/Exercicio1.java
+++ Aula_02/Exercicio1.java
@@ -1,1 +1,12 @@
 
+public class Apresentacao {
+    public static void main(String[] args) {
+        String nome = "Vinicius";
+        int idade = 23;
+        double altura = 1.75;
+        boolean estudaNoite = true;
+
+        System.out.printf("Olá, me chamo %s, tenho %d anos, minha altura é %.2f e estudo à noite: %b.%n", 
+                          nome, idade, altura, estudaNoite);
+    }
+}
