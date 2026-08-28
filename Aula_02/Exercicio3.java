300dceafe65001d539d9de729566fff40bf5f1b0
SlickBunny <163926968+SlickBunny@users.noreply.github.com>
2026-08-28 11:25:29 -0300

    Create Exercicio3.java

---
+++ Aula_02/Exercicio3.java
@@ -0,0 +1,15 @@
+public class Previsao {
+    public static void main(String[] args) {
+
+        System.out.println(7 / 2);
+
+        System.out.println(7 / 2.0);
+
+        System.out.println(7 % 2);
+        
+        System.out.println((int) 7.9);
+        
+        System.out.println(10 / 4 * 4);
+      
+    }
+}
